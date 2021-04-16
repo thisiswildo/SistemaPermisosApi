35b53fec44b478be1b61ad23ba06be01c188f10e
Wildo Enrique Ruiz González <wildo1997_@hotmail.com>
2021-04-16 17:21:54 -0400

    Adding Entities Mapping

---
+++ SistemaPermisos.Data/Mapping/PermisoMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaPermisos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPermisos.Data.Mapping
+{
+    public class PermisoMap : IEntityTypeConfiguration<Permiso>
+    {
+        public void Configure(EntityTypeBuilder<Permiso> builder)
+        {
+            builder.ToTable(nameof(Permiso))
+                .HasKey(x => x.Id);
+        }
+    }
+}
---
+++ SistemaPermisos.Data/Mapping/TipoPermisoMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaPermisos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPermisos.Data.Mapping
+{
+    public class TipoPermisoMap : IEntityTypeConfiguration<TipoPermiso>
+    {
+        public void Configure(EntityTypeBuilder<TipoPermiso> builder)
+        {
+            builder.ToTable(nameof(TipoPermiso))
+                .HasKey(x => x.Id);
+        }
+    }
+}
