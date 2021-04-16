9c46299e241603d9846860711434445d045c1374
Wildo Enrique Ruiz González <wildo1997_@hotmail.com>
2021-04-16 17:37:01 -0400

    Adding IServiceCollection.AddControllers to the ConfigureServices method

--- SistemaPermisos.Api/Startup.cs
+++ SistemaPermisos.Api/Startup.cs
@@ -26,6 +26,8 @@ namespace SistemaPermisos.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("Conexion"));
