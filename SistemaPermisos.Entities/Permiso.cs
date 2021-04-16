5dd002a94fa9b039ece5df7ac38373f042bd8058
Wildo Enrique Ruiz González <wildo1997_@hotmail.com>
2021-04-16 17:05:04 -0400

    Adding entities of the project

---
+++ SistemaPermisos.Entities/Permiso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SistemaPermisos.Entities
+{
+    public class Permiso
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NombreEmpleado { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ApellidosEmpleado { get; set; }
+
+        [Required]
+        public int TipoPermisoId { get; set; }
+
+        [Required]
+        public DateTime FechaPermiso { get; set; }
+
+
+        public TipoPermiso TipoPermiso { get; set; }
+    }
+}
---
+++ SistemaPermisos.Entities/TipoPermiso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SistemaPermisos.Entities
+{
+    public class TipoPermiso
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public int Descripcion { get; set; }
+
+
+        public ICollection<Permiso> Permisos { get; set; }
+    }
+}
