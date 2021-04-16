d573cb2430d2c4e527ea57b2f5f7e4b5f1a006be
Wildo Enrique Ruiz González <wildo1997_@hotmail.com>
2021-04-16 17:21:05 -0400

    Adding Application Db Context

---
+++ SistemaPermisos.Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaPermisos.Data.Mapping;
+using SistemaPermisos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPermisos.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new PermisoMap());
+            builder.ApplyConfiguration(new TipoPermisoMap());
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Permiso> Permisos { get; set; }
+        public DbSet<TipoPermiso> TiposPermisos { get; set; }
+    }
+}
